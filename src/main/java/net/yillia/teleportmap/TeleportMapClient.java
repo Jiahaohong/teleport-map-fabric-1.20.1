385142cbec0b0b0a5f2e25dda1bed4bacd37741c
GPULAP <hjh1721957623@outlook.com>
2023-12-30 14:41:22 +0800

    init

---
+++ src/main/java/net/yillia/teleportmap/TeleportMapClient.java
@@ -0,0 +1,10 @@
+package net.yillia.teleportmap;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TeleportMapClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/yillia/teleportmap/TeleportMapDataGenerator.java
@@ -0,0 +1,11 @@
+package net.yillia.teleportmap;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TeleportMapDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
